d528d9bc54ec1291c991896e4e38e4f16fb491c1
cvfranco <101279381+cvfranco@users.noreply.github.com>
2022-08-04 14:55:27 +0200

    Update Python Strategies

--- Strategies in Python/Simple_MA/main.py
+++ Strategies in Python/Simple_MA/main.py
@@ -13,27 +13,38 @@ import pathlib
 import sys
 
 parent_folder = str(pathlib.PurePath(os.getcwd()).parent)
-strategy_folder = parent_folder + "\\Strategies in Python\\Simple_MA"
+strategy_folder = parent_folder + "r\Strategies in Python\Simple_MA"
 sys.path.append(strategy_folder)
 # ----------------------------------------------------------------------------------- #
 
 # Import needed packages
 
-from Asset import *
-
+from Asset import Asset
+import pandas as pd
 
 # ------------------------------------- Main ---------------------------------------- #
 
-amazon = Asset('AMZN', 'Stock', 'Technology', 'US')
-data = amazon.market_data()
-sma = amazon.simple_moving_average(8)
-ema = amazon.exp_moving_average(21)
+# Test one asset ----------------------------------------------------------------------:
+## instanciate an object
+asset = Asset('AAP', 'Stock', 'Technology', 'US')
+## Gets the data on the csv files --> we could use the yf API to get more updated data
+data = asset.market_data()
+# Calculate the Moving averages: Simple SMA(p) and Exponential EMA(p)
+sma = asset.simple_moving_average(8)
+ema = asset.exp_moving_average(21)
 
-df_test = pd.concat([amazon.simple_moving_average(8),
-                     amazon.simple_moving_average(21).iloc[:,2]], axis = 1, ignore_index=False)
-amazon.plot_data(df_test, '2012-12-10')
+# Plot the price and the moving averages
+df_test = pd.concat([asset.simple_moving_average(8),
+                     asset.simple_moving_average(21).iloc[:,2],
+                     asset.simple_moving_average(200).iloc[:,2]], axis = 1, ignore_index=False)
+asset.plot_data(df_test)
+
+# Get the results for each asset on both Buy and Sell
+results_2MA = asset.strategy_MMA('EMA', [8, 21])
+results_3MA = asset.strategy_MMA('EMA', [8, 21, 200])
+#-----------------------------------------------------------------------------------------
 
 
-results = amazon.strategy_MMA('EMA', [8, 21])
-
-
+# Test all assets ----------------------------------------------------------------------:
+data_folder = parent_folder + r'\Strategies in Python\data\assets'
+assets_names = [x.replace(".csv","")  for x in os.listdir(data_folder)] 
